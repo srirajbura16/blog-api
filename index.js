df0c734bd93c4a799227e6ccee81f562e96b500d
Sriraj Bura <sriraj.bura16@gmail.com>
2021-09-22 13:26:45 -0400

    implemented database setup

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//Mongoose connection
+const mongoose = require('mongoose');
+const mongoDB = process.env.DB_HOST;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+//Middleware
 app.use(express.json());
 app.use(express.urlencoded());
 
+//Router
 app.use('/api', require('./routes/api'));
 
+//
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+require('dotenv').config();
+
 router.get('/', (req, res) => {
-  res.send('this worked?');
+  res.send('GET ALL POSTS');
+});
+
+router.post('/', (req, res) => {
+  res.send('MAKE NEW POST');
 });
 
 router.get('/:postId', (req, res) => {
-  res.send(`Get post: ${req.params.postId}`);
+  res.send(`GET POST BASED ON ID`);
+});
+
+router.delete('/:postId', (req, res) => {
+  res.send(`DELETE POST BASED ON ID`);
 });
 // router.get('/:postID', posts_controller.post_get);
 
